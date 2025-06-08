5e1aa9c8aad31f1a0064d59959b082f110c6f97e
ahmedzayed4596 <ahmedzayed101177@gmail.com>
2025-06-08 20:52:55 +0300

    ta3ala b dahrak

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Nav from "./Component/Nav/Nav";
 import Home from "./Component/Home/Home";
-
+import { createBrowserRouter } from "react-router-dom";
+createBrowserRouter
 function App() {
   const [count, setCount] = useState(0);
 
