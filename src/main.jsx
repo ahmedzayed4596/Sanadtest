6fb8c6282b7d89ca202391e9b942938e417627db
ahmedzayed4596 <ahmedzayed101177@gmail.com>
2025-06-10 00:16:53 +0300

    back

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App.jsx";
-import "./index.css"; // ✅ this should exist in src/
-import "flowbite"; // ✅ this will be bundled by Vite
+import "flowbite-react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+z
