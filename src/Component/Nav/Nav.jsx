8611c378febcfa40ebed0f6c3b5ed65520e2a21b
aboelazm3 <mostafaaboelazm939@gmail.com>
2025-06-11 19:16:55 +0300

    gh

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -14,45 +14,15 @@ export default function Nav() {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src={logo2}
+              src={logo}
               className="h-16 hover:cursor-pointer"
               alt="Sanad Logo"
             />
           </Link>
-          <div className="flex md:order-2 gap-2 space-x-3 md:space-x-0">
-            <Link
-              to={"/Register"}
-              className="text-white bg-[#07543a] hover:bg-[#07543ab2] hover:cursor-pointer transition-colors duration-200 ease-in-out font-medium rounded-lg text-sm px-4 py-2"
-            >
-              إنشاء حساب
-            </Link>
-            <Link
-              to={"/Singin"}
-              className="text-white bg-[#07543a] hover:bg-[#07543ab2] hover:cursor-pointer transition-colors duration-200 ease-in-out font-medium rounded-lg text-sm px-4 py-2"
-            >
-              تسجيل دخول
-            </Link>
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden"
-            >
-              <svg className="w-5 h-5" fill="none" viewBox="0 0 17 14">
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
-
-          <div
-            className={`${
+           <div
+            className={` ${
               isOpen ? "block" : "hidden"
-            } w-full lg:flex lg:w-auto lg:order-1`}
+            } w-full lg:flex lg:w-auto `}
           >
             <ul className="flex flex-col p-4 lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg  lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-white">
               <li>
@@ -97,6 +67,46 @@ export default function Nav() {
               </li>
             </ul>
           </div>
+
+ 
+
+
+ <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex md:order-2 items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden"
+            >
+              <svg className="w-5 h-5" fill="none" viewBox="0 0 17 14">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+
+            <div className="flex md:order-1 gap-2 space-x-3 lg:space-x-0">
+            <Link
+              to={"/Register"}
+              className="text-white bg-[#07543a] hover:bg-[#07543ab2] hover:cursor-pointer transition-colors duration-200 ease-in-out font-medium rounded-lg text-sm px-4 py-2"
+            >
+              إنشاء حساب
+            </Link>
+            <Link
+              to={"/Singin"}
+              className="text-white bg-[#07543a] hover:bg-[#07543ab2] hover:cursor-pointer transition-colors duration-200 ease-in-out font-medium rounded-lg text-sm px-4 py-2"
+            >
+              تسجيل دخول
+            </Link>
+           
+          </div>
+
+
+         
+
+         
         </div>
         <div className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-l from-yellow-300 via-yellow-400 to-yellow-600"></div>
       </nav>
