512534961f4493cf13501ab8ebb19af66ae29d0b
ahmedzayed4596 <ahmedzayed101177@gmail.com>
2025-06-11 11:15:40 +0300

    changed to 300 px

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -4,7 +4,7 @@ import signupImg from "../../assets/singup.png";
 export default function Register() {
   return (
     <>
-      <div className="pt-40 px-7 ">
+      <div className="pt-40 px-7 h-screen">
         <div className="container mx-auto">
           <div className="grid lg:grid-cols-2">
             <div className="col-span-1">
@@ -16,7 +16,7 @@ export default function Register() {
             <div className="md:col-span-1 lg:block hidden">
               <img
                 src={signupImg}
-                height="500px"
+                height="300px"
                 className="w-full  object-cover rounded-lg shadow-2xl"
                 alt=""
               />
