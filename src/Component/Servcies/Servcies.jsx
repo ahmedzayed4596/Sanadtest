e7c9b09b7a1843e1435221a8f02cbeb76b124b55
aboelazm3 <mostafaaboelazm939@gmail.com>
2025-06-13 03:06:28 +0300

    ser

--- src/Component/Servcies/Servcies.jsx
+++ src/Component/Servcies/Servcies.jsx
@@ -1,9 +1,133 @@
-import React, { useState } from 'react'
-import style from './Servcies.module.css'
+import React, { useState } from "react";
+import style from "./Servcies.module.css";
+import Slider from "react-slick";
+import gosi from '../../assets/gosi.png'
+import absher from '../../assets/absher.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Servcies() {
+
+    const settings = {
+    dots: false,
+    arrows : true ,
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 5000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+  
     
-return <>
-    <h1>Servcies Component</h1>
-</>
+  return (
+    <>
+      <div className="pt-40">
+        <h2 className="text-5xl text-center font-medium mb-4">
+          الخدمات التي نقدمها في{" "}
+          <span className="font-bold text-green-800">سند</span>
+        </h2>
+        <div className=" w-[90%] bg-gray-100 rounded-2xl  mx-auto">
+       
+
+
+
+            <div className="slider-container">
+      <Slider {...settings}>
+       
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5"> التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] h-auto w-auto rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات أبشر </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={absher} className="w-full h-[100px]" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+       <div className=" p-4">
+                <h2 className="text-center text-[#f0bb35] text-2xl my-3.5">خدمات التأمينات الاجتماعية </h2>
+              <div className="bg-[#07543a] rounded-full">
+
+                <img src={gosi} className="w-full" alt="" />
+              </div>
+            </div>
+      </Slider>
+    </div>
+
+         
+
+       
+        </div>
+      </div>
+    </>
+  );
 }
